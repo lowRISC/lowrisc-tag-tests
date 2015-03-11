ebd0f8543a7496fe45ab45e05d8a564bf128991e
Wei Song <wsong83@gmail.com>
2015-03-11 15:00:05 +0000

    record the FPGA benchmark results

---
+++ ccbench/ccbench_fpga.sh
@@ -0,0 +1,251 @@
+#/bin/bash
+
+~/fesvr-zynq ~/pk ./caches 4 300100 1 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 16 300100 1 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 32 300100 1 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 64 300100 1 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 512 300100 1 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 1024 300100 1 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 2048 300100 1 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 4192 300100 1 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 5000 300100 1 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 6000 300100 1 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 7000 300100 1 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 8192 300100 1 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 9000 300100 1 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 10000 300100 1 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 12000 300100 1 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 16384 300100 1 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 24576 300100 1 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 29696 300100 1 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 32768 300100 1 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 35840 300100 1 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 37000 300100 1 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 40000 300100 1 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 50100 300100 1 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 60000 300100 1 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 62000 300100 1 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 65536 300100 1 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 68000 300100 1 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 70000 300100 1 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 75100 300100 1 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 100000 300100 1 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 131072 300100 1 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 229376 300100 1 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 262144 300100 1 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 300100 300100 1 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 400100 300100 1 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 4 300100 16 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 16 300100 16 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 32 300100 16 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 64 300100 16 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 512 300100 16 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 1024 300100 16 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 2048 300100 16 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 4192 300100 16 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 5000 300100 16 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 6000 300100 16 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 7000 300100 16 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 8192 300100 16 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 9000 300100 16 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 10000 300100 16 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 12000 300100 16 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 16384 300100 16 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 24576 300100 16 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 29696 300100 16 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 32768 300100 16 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 35840 300100 16 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 37000 300100 16 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 40000 300100 16 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 50100 300100 16 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 60000 300100 16 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 62000 300100 16 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 65536 300100 16 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 68000 300100 16 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 70000 300100 16 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 75100 300100 16 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 100000 300100 16 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 131072 300100 16 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 229376 300100 16 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 262144 300100 16 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 300100 300100 16 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 400100 300100 16 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 4 300100 0 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 16 300100 0 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 32 300100 0 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 64 300100 0 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 512 300100 0 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 1024 300100 0 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 2048 300100 0 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 4192 300100 0 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 5000 300100 0 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 6000 300100 0 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 7000 300100 0 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 8192 300100 0 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 9000 300100 0 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 10000 300100 0 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 12000 300100 0 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 16384 300100 0 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 24576 300100 0 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 29696 300100 0 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 32768 300100 0 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 35840 300100 0 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 37000 300100 0 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 40000 300100 0 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 50100 300100 0 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 60000 300100 0 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 62000 300100 0 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 65536 300100 0 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 68000 300100 0 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 70000 300100 0 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 75100 300100 0 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 100000 300100 0 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 131072 300100 0 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 229376 300100 0 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 262144 300100 0 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 300100 300100 0 >> ccbench_caches.result
+~/fesvr-zynq ~/pk ./caches 400100 300100 0 >> ccbench_caches.result
+
+
+~/fesvr-zynq ~/pk ./band_req 1 4 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 1 16 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 1 32 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 1 64 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 1 512 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 1 1024 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 1 2048 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 1 4192 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 1 5000 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 1 6000 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 1 8192 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 1 16384 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 1 50100 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 1 75100 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 1 100000 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 1 262144 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 1 300100 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 2 4 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 2 16 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 2 32 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 2 64 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 2 512 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 2 1024 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 2 2048 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 2 4192 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 2 5000 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 2 6000 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 2 8192 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 2 16384 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 2 50100 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 2 75100 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 2 100000 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 2 262144 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 2 300100 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 4 4 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 4 16 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 4 32 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 4 64 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 4 512 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 4 1024 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 4 2048 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 4 4192 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 4 5000 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 4 6000 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 4 8192 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 4 16384 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 4 50100 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 4 75100 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 4 100000 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 4 262144 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 4 300100 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 8 4 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 8 16 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 8 32 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 8 64 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 8 512 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 8 1024 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 8 2048 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 8 4192 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 8 5000 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 8 6000 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 8 8192 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 8 16384 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 8 50100 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 8 75100 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 8 100000 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 8 262144 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 8 300100 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 16 4 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 16 16 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 16 32 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 16 64 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 16 512 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 16 1024 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 16 2048 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 16 4192 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 16 5000 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 16 6000 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 16 8192 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 16 16384 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 16 50100 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 16 75100 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 16 100000 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 16 262144 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 16 300100 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 20 4 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 20 16 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 20 32 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 20 64 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 20 512 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 20 1024 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 20 2048 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 20 4192 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 20 5000 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 20 6000 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 20 8192 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 20 16384 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 20 50100 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 20 75100 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 20 100000 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 20 262144 300100 >> ccbench_band_req.result
+~/fesvr-zynq ~/pk ./band_req 20 300100 300100 >> ccbench_band_req.result
+
+
+~/fesvr-zynq ~/pk ./mem_interleaving 1 32 67108864 >> ccbench_mem_interleaving.result
+~/fesvr-zynq ~/pk ./mem_interleaving 1 64 67108864 >> ccbench_mem_interleaving.result
+~/fesvr-zynq ~/pk ./mem_interleaving 1 1024 67108864 >> ccbench_mem_interleaving.result
+~/fesvr-zynq ~/pk ./mem_interleaving 1 4096 67108864 >> ccbench_mem_interleaving.result
+~/fesvr-zynq ~/pk ./mem_interleaving 1 8192 67108864 >> ccbench_mem_interleaving.result
+~/fesvr-zynq ~/pk ./mem_interleaving 1 16384 67108864 >> ccbench_mem_interleaving.result
+~/fesvr-zynq ~/pk ./mem_interleaving 2 32 67108864 >> ccbench_mem_interleaving.result
+~/fesvr-zynq ~/pk ./mem_interleaving 2 64 67108864 >> ccbench_mem_interleaving.result
+~/fesvr-zynq ~/pk ./mem_interleaving 2 1024 67108864 >> ccbench_mem_interleaving.result
+~/fesvr-zynq ~/pk ./mem_interleaving 2 4096 67108864 >> ccbench_mem_interleaving.result
+~/fesvr-zynq ~/pk ./mem_interleaving 2 8192 67108864 >> ccbench_mem_interleaving.result
+~/fesvr-zynq ~/pk ./mem_interleaving 2 16384 67108864 >> ccbench_mem_interleaving.result
+~/fesvr-zynq ~/pk ./mem_interleaving 4 32 67108864 >> ccbench_mem_interleaving.result
+~/fesvr-zynq ~/pk ./mem_interleaving 4 64 67108864 >> ccbench_mem_interleaving.result
+~/fesvr-zynq ~/pk ./mem_interleaving 4 1024 67108864 >> ccbench_mem_interleaving.result
+~/fesvr-zynq ~/pk ./mem_interleaving 4 4096 67108864 >> ccbench_mem_interleaving.result
+~/fesvr-zynq ~/pk ./mem_interleaving 4 8192 67108864 >> ccbench_mem_interleaving.result
+~/fesvr-zynq ~/pk ./mem_interleaving 4 16384 67108864 >> ccbench_mem_interleaving.result
+
+
+~/fesvr-zynq ~/pk ./strided 1 32 1 50 >> ccbench_strided.result
+~/fesvr-zynq ~/pk ./strided 1 32 2 50 >> ccbench_strided.result
+~/fesvr-zynq ~/pk ./strided 1 32 4 50 >> ccbench_strided.result
+~/fesvr-zynq ~/pk ./strided 1 64 1 50 >> ccbench_strided.result
+~/fesvr-zynq ~/pk ./strided 1 64 2 50 >> ccbench_strided.result
+~/fesvr-zynq ~/pk ./strided 1 64 4 50 >> ccbench_strided.result
+~/fesvr-zynq ~/pk ./strided 1 128 1 50 >> ccbench_strided.result
+~/fesvr-zynq ~/pk ./strided 1 128 2 50 >> ccbench_strided.result
+~/fesvr-zynq ~/pk ./strided 1 128 4 50 >> ccbench_strided.result
+~/fesvr-zynq ~/pk ./strided 1 256 1 50 >> ccbench_strided.result
+~/fesvr-zynq ~/pk ./strided 1 256 2 50 >> ccbench_strided.result
+~/fesvr-zynq ~/pk ./strided 1 256 4 50 >> ccbench_strided.result
+~/fesvr-zynq ~/pk ./strided 1 512 1 50 >> ccbench_strided.result
+~/fesvr-zynq ~/pk ./strided 1 512 2 50 >> ccbench_strided.result
+~/fesvr-zynq ~/pk ./strided 1 512 4 50 >> ccbench_strided.result
+~/fesvr-zynq ~/pk ./strided 1 1024 1 50 >> ccbench_strided.result
+~/fesvr-zynq ~/pk ./strided 1 1024 2 50 >> ccbench_strided.result
+~/fesvr-zynq ~/pk ./strided 1 1024 4 50 >> ccbench_strided.result
